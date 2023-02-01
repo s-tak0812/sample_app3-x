4bb06014c0dba947cfb41fdb1c44428d34c0fabd
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-01 14:50:00 +0000

    [Finish]3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,6 +8,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
 
     if @list.save
+      flash[:notice]="投稿が成功しました"
       redirect_to list_path(@list.id)
     else
       render :new
